2953c95f93cd14c7d8e867131d28912eebce754e
Hazel Shih <hazel@25sprout.com>
2025-01-14 14:56:54 +0800

    add about content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,15 @@
 export default function AboutLandingPage() {
   return (
     <div>
-      <h1>Welcome to the About</h1>
-      <p>I am a lovely person.</p>
+      <h1>嗨，我是 Hazel 👋🏻</h1>
+      <h1>有些朋友說「黑走」比較好記好唸</h1>
+      <br />
+      <p>
+        任何想說的都可以來信到：
+        <span>
+          <a href="mailto:hazelwonder.me@gmail.com">hazelwonder.me@gmail.com</a>
+        </span>
+      </p>
     </div>
   );
 }
